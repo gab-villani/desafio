3429edd7a5d0d89abc24dcbd0d2a00f1f6e82356
Douglas-Vinicius232 <douglas.vinic77@gmail.com>
2023-06-26 16:47:01 -0400

    [feat] logica completa só falta a parte criaiva

--- main.py
+++ main.py
@@ -1,32 +1,33 @@
 import sounddevice as sd
 import time
 
-def tocar_musica():
-    filename = "../Cinco Patinhos.mp3"  # Caminho para o arquivo de áudio
+# def tocar_musica():
+#     filename = "../Cinco Patinhos.mp3"  # Caminho para o arquivo de áudio
 
-    # Carrega o arquivo de áudio
-    data, fs = sd.read(filename, dtype='float32')
+#     Carrega o arquivo de áudio
+#     data, fs = sd.read(filename, dtype='float32')
     
-    # Reproduz o áudio
-    sd.play(data, fs)
+#     Reproduz o áudio
+#     sd.play(data, fs)
 
 def musica_patinhos():
     quantidade = int(input("Digite a quantidade inicial de patinhos: "))
-
-    while quantidade > 0:
-        print(f"{quantidade} patinhos foram passear")
+    qtd = quantidade
+    while qtd > 0:
+        
+        print(f"\n{qtd} patinhos foram passear")
         print("Além das montanhas para brincar")
         print("A mamãe gritou: 'Quá, quá, quá, quá!'")
-        quantidade -= 1
+        qtd -= 1
 
-        if quantidade > 0:
-            print(f"Mas só {quantidade} patinhos voltaram de lá\n")
+        if qtd > 0:
+            print(f"Mas só {qtd} patinhos voltaram de lá\n")
         else:
-            print("E nenhum patinho voltou de lá\n")
+            print(f"\nA mamãe patinha foi procurar \nAlém das montanhas onde foi parar\nA mamãe gritou: 'Quá, quá, quá, quá!'\nE os {quantidade} patinhos voltaram de lá")
 
-        time.sleep(1)  # Atraso de 1 segundo entre as linhas
+        time.sleep(6)  # Atraso de 1 segundo entre as linhas
 
-    sd.stop()
+    # sd.stop()
 
-tocar_musica()
+# tocar_musica()
 musica_patinhos()
